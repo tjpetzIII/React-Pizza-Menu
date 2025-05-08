9e95d43b0d19fc3134026ba0ba0b133eedfec6e4
Thomas Petz <tompetz2000@gmail.com>
2025-05-08 07:04:04 -0400

    Added conditional rendering with ternaries

--- src/index.js
+++ src/index.js
@@ -71,12 +71,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p> We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -105,11 +107,15 @@ function Footer() {
   // else alert("Sorry we're closed");
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   );
