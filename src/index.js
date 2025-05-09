63e64a1a832ac98c189c1a33b6e7fc6834e8a6c1
Thomas Petz <tompetz2000@gmail.com>
2025-05-09 06:39:06 -0400

    Added conditional rendering with mulitple returns

--- src/index.js
+++ src/index.js
@@ -85,6 +85,7 @@ function Menu() {
 }
 function Pizza(props) {
   console.log(props);
+  if (props.pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -105,6 +106,12 @@ function Footer() {
   console.log(isOpen);
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!")
   // else alert("Sorry we're closed");
+  // if (!isOpen)
+  //   return (
+  //     <p>
+  //       We're happy to welcome you between {openHour}:00 and {closeHour}:00
+  //     </p>
+  //   );
   return (
     <footer className="footer">
       {isOpen ? (
